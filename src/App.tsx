56fef274f46811cc5e1930912ef3601b4c91fa1a
Leeeeeeeeeeeeeeeeeeeee <dimarodin0@gmail.com>
2021-03-13 19:24:56 +0400

    ((adaptive design))

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,39 @@ import Grid from '@material-ui/core/Grid';
 import { ImageRedactor } from './view/ImageRedactor';
 import { CoordsArrayContext } from './CoordsArrayContext';
 import { CodeViewer } from './view/CodeViewer';
+import { DrawPreview } from './view/DrawPreview';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
             background: '#616161',
-            height: window.innerHeight + "px",
-            width: window.screen.width + "px",
+            height: "100vh",
+            width: "100vw",
             padding: "20px",
             boxSizing: "border-box",
             margin: 0,
         },
         paper: {
-            padding: theme.spacing(2),
+            padding: "2vh",
             display: 'flex',
             alignItems: 'center',
+            overflow: "hidden",
             flexDirection: "column",
             justifyContent: 'center',
             color: theme.palette.text.secondary,
-            height: (window.innerHeight - 80) + "px",
+            height: "90vh",
+        },
+
+        halfPapper: {
+            padding: "2vh",
+            display: 'flex',
+            alignItems: 'center',
+            overflow: "hidden",
+            flexDirection: "column",
+            justifyContent: 'center',
+            color: theme.palette.text.secondary,
+            height: "40vh",
+            marginTop: "2vh",
         },
     }),
 );
@@ -37,14 +51,17 @@ export default function App() {
         <div className={classes.root}>
             <CoordsArrayContext.Provider value={{ coords, setCoords }} >
                 <Grid container spacing={3}>
-                    <Grid item xs={12} sm={6}>
+                    <Grid item xs={6}>
                         <Paper className={classes.paper}>
                             <ImageRedactor />
                         </Paper>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Paper className={classes.paper}>
+                    <Grid item xs={6}>
+                        <Paper className={classes.halfPapper}>
                             <CodeViewer />
+                        </Paper>
+                        <Paper className={classes.halfPapper}>
+                            <DrawPreview />
                         </Paper>
                     </Grid>
                 </Grid>
